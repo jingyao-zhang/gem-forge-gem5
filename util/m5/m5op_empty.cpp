54a784aad4df92bab64bec5f725b346c40e79908
seanzw <seanyukigeek@gmail.com>
2019-10-03 17:58:48 -0700

    [feat] Volatile load in empty m5op implementation.
    Change-Id: Ia3d6262adf9a78c0f13589dfac304f0dea1daa4b

--- util/m5/m5op_empty.cpp
+++ util/m5/m5op_empty.cpp
@@ -1,7 +1,27 @@
 #include "stdint.h"
+
+namespace {
+/**
+ * Read this to prevent it being optimized away.
+ */
+volatile int dummy;
+} // namespace
+
 extern "C" {
-void m5_reset_stats(uint64_t ns_delay, uint64_t ns_period) {}
-void m5_dump_stats(uint64_t ns_delay, uint64_t ns_period) {}
-void m5_work_begin(uint64_t workid, uint64_t threadid) {}
-void m5_work_end(uint64_t workid, uint64_t threadid) {}
+__attribute__((noinline)) extern void m5_reset_stats(uint64_t ns_delay,
+                                                     uint64_t ns_period) {
+  volatile int x = dummy;
+}
+__attribute__((noinline)) extern void m5_dump_stats(uint64_t ns_delay,
+                                                    uint64_t ns_period) {
+  volatile int x = dummy;
+}
+__attribute__((noinline)) extern void m5_work_begin(uint64_t workid,
+                                                    uint64_t threadid) {
+  volatile int x = dummy;
+}
+__attribute__((noinline)) extern void m5_work_end(uint64_t workid,
+                                                  uint64_t threadid) {
+  volatile int x = dummy;
+}
 }
