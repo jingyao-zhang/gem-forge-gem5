74e9cc9aea79a7fcd05b28b4b17788be8b957295
Zhengrong Wang <seanyukigeek@gmail.com>
2020-02-03 18:13:34 -0800

    [feat] More AVX inst.
    vmovd.
    
    Change-Id: Ic37b0e8e36edde47c84ca433ec01bed040ff5403

--- src/arch/x86/isa/insts/simd512/floating_point/data_transfer/vmovd.py
+++ src/arch/x86/isa/insts/simd512/floating_point/data_transfer/vmovd.py
@@ -1,5 +1,21 @@
 microcode = '''
 
+def macroop VMOVD_XMM_R {
+    mov2fp xmm0, regm, srcSize=dsz, destSize=8
+    vclear dest=xmm1, destVL=8
+};
+
+def macroop VMOVD_XMM_M {
+    ldfp xmm0, seg, sib, disp, dataSize=dsz
+    vclear dest=xmm1, destVL=8
+};
+
+def macroop VMOVD_XMM_P {
+    rdip t7
+    ldfp xmm0, seg, riprel, disp, dataSize=dsz
+    vclear dest=xmm1, destVL=8
+};
+
 def macroop VMOVD_R_XMM {
     mov2int reg, xmm0m, size=dsz
 };
