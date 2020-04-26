b714de63a007a18011aa81ad80c1acd2bec2a610
seanzw <seanyukigeek@gmail.com>
2020-04-25 23:22:32 -0700

    [fix] Make max_abs_tick work for GemForge.
    Change-Id: Ic6e11e5a61899ac4a3535100e4ad23598135a57c

--- configs/example/gem_forge/GemForgeSystem.py
+++ configs/example/gem_forge/GemForgeSystem.py
@@ -7,11 +7,12 @@ def run(options, root, system, future_cpus):
     if future_cpus:
         future_cpus[0].max_insts_any_thread = 1e9
     m5.instantiate(checkpoint_dir)
+    max_tick = options.abs_max_tick if options.abs_max_tick else m5.MaxTick
     if future_cpus:
         assert(len(future_cpus) == len(system.cpu))
         # Fast forward simulation.
         print('**** FAST FORWARD SIMULATION ****')
-        exit_event = m5.simulate()
+        exit_event = m5.simulate(max_tick)
         print('**** Switch CPUS @ tick {t} as {s} ****'.format(
             t=m5.curTick(), s=exit_event.getCause()))
 
@@ -20,7 +21,8 @@ def run(options, root, system, future_cpus):
         m5.switchCpus(system, switch_cpu_list)
         m5.stats.reset()
     print('**** REAL SIMULATION ****')
-    exit_event = m5.simulate(options.abs_max_tick)
+    assert(max_tick > m5.curTick())
+    exit_event = m5.simulate(max_tick - m5.curTick())
     print('**** Exit @ tick {t} as {s} ****'.format(
         t=m5.curTick(), s=exit_event.getCause()))
     if exit_event.getCode() != 0:
